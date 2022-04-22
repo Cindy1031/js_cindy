57d6c515ddcc063a6b14e72ee53d429cb1a90783
Cindy1031 <98787094+Cindy1031@users.noreply.github.com>
2022-04-21 21:59:12 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,17 @@
+fuction evenorodd()
+console.log("thisworks");
+
+
+
+{  for(var i = 1; i < 21; i++)
+  {
+    if ((i)%2 == 0)
+      console.log("i=" i + "is even");
+    else
+      console.log("i=" i + "is odd");
+  }
+}
+console.log(evenorodd);
+
+document.getElementById("my_div").innerHTML = fuction evenorodd();
+}
